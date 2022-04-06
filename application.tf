56b9f7a1fa320688c31b23040f2d8d5cdfc5a76c
Rahul Jha <rahuljha600@gmail.com>
2022-04-06 11:31:23 +0530

    check auto

--- application.tf
+++ application.tf
@@ -17,8 +17,8 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
 }
-resource "okta_app_saml" "TerraFromTestApplicationCheckfinalUpdate" {
-  label                    = "TerraFromTestApplicationCheckfinalUpdate"
+resource "okta_app_saml" "TerraFromTestApplicationCheckAuto" {
+  label                    = "TerraFromTestApplicationCheckAuto"
   sso_url                  = "https://example.com"
   recipient                = "https://example.com"
   destination              = "https://example.com"
